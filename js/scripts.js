6db2dbe0e27e9f6a41ad13d723679a31ad969510
Petr Petukhov <kotov0991@gmail.com>
2018-04-23 22:52:46 +0300

    js fix mmyy input mask

--- js/scripts.js
+++ js/scripts.js
@@ -31,7 +31,7 @@ $(function() {
 	});
 
 	$('.card-number').mask("0000 0000 0000 0000");
-	$('.card-date').mask("00 / AZ", {
+	$('.card-date').mask("AZ / 00", {
 		translation: {
 			'A': {
 				pattern: /[0-1]/, optional: true	
